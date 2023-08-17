30b80a9290d293ea7ded5690143170fda309ac76
Andrei Vishniakov <31008759+avishniakov@users.noreply.github.com>
2023-08-17 11:23:09 +0200

    fix 3.8 Annotated issue

--- template/steps/etl/train_data_splitter.py
+++ template/steps/etl/train_data_splitter.py
@@ -1,7 +1,8 @@
 # {% include 'template/license_header' %}
 
 
-from typing import Annotated, Tuple
+from typing import Tuple
+from typing_extensions import Annotated
 
 import pandas as pd
 from sklearn.model_selection import train_test_split

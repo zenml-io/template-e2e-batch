9dd228984e85a662ad9a722f618746d136c6c436
Andrei Vishniakov <31008759+avishniakov@users.noreply.github.com>
2023-10-18 16:49:16 +0200

    rename

--- template/steps/promotion/__init__.py
+++ template/steps/promotion/__init__.py
@@ -3,8 +3,9 @@
 
 {%- if metric_compare_promotion %}
 from .promote_get_metric import promote_get_metric
-from .promote_metric_compare_promoter import promote_metric_compare_promoter
+from .promote_metric_compare_promoter_in_model_registry import promote_metric_compare_promoter_in_model_registry
 {%- else %}
-from .promote_latest import promote_latest
+from .promote_latest_in_model_registry import promote_latest_in_model_registry
 {%- endif %}
 from .promote_get_versions import promote_get_versions
+from .promote_model_version_in_model_control_plane import promote_model_version_in_model_control_plane
